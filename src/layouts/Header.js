0412a876549aee57a0ff74547e2face4df6b95c7
gonnajoong <gonnajoong@gmail.com>
2021-03-24 17:52:02 +0900

    파일 구조 수정중

---
+++ src/layouts/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount (){}
+
+    render(){
+        return (
+            <header>헤더</header>
+        );
+    }
+}
+
+export default Header;
