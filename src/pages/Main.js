3b51cab6351f3c48f8810a8b05e5d103944f5f61
gonnajoong <gonnajoong@gmail.com>
2021-03-28 12:15:30 +0900

    포트폴리오 마크업 1차 작성 완료

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -52,24 +52,39 @@ class Main extends Component {
                         </p>
                     </h1>
                     <ul className="gj-about-list">
-                        <h2>학력</h2>
-                        <li>
-                            <strong>화성 남양고등학교</strong>
-                            <span>2012. 03 ~ 2015. 02</span>
-                            <p>고등학교 졸업</p>
+                        <h2>내가 걸어온 길</h2>
+                        <li className="gj-split-wrap">
+                            <span className="gj-split-image-wrap">
+                                <img src="#" alt="about-image"/>
+                            </span>
+                            <span className="gj-split-text-wrap gj-split-padding">
+                                <h2>화성 남양고등학교</h2>
+                                <span>2012. 03 ~ 2015. 02</span>
+                                <p>고등학교 3년 개근으로 졸업</p>
+                            </span>
                         </li>
-                        <li>
-                            <strong>안산 라인컴퓨터아트학원</strong>
-                            <span>2018. 04 ~ 2019. 02</span>
-                            <p>국비지원과정으로 정상 수료</p>
+                        <li className="gj-split-wrap">
+                            <span className="gj-split-text-wrap ">
+                                <h2>안산 라인컴퓨터아트학원</h2>
+                                <span>2018. 04 ~ 2019. 02</span>
+                                <p>국비지원으로 스마트 UI / UX과정 교육 정상 수료</p>
+                            </span>
+                            <span className="gj-split-image-wrap">
+                                <img src="#" alt="about-image"/>
+                            </span>
                         </li>
-                    </ul>
-                    <ul className="gj-about-list">
-                        <h2>경력</h2>
-                        <li>
-                            <strong>(주)린코</strong>
-                            <span>2019. 05. 20 ~ 2020. 08. 31</span>
-                            <p>웹 퍼블리셔 및 웹 프론트엔드로 근무</p>
+                        <li className="gj-split-wrap">
+                            <span className="gj-split-image-wrap">
+                                <img src="#" alt="about-image"/>
+                            </span>
+                            <span className="gj-split-text-wrap  gj-split-padding">
+                                <h2>(주)린코</h2>
+                                <span>2019. 05. 20 ~ 2020. 08. 31</span>
+                                <p>
+                                    스타트업 기업에서 라라벨 및 리액트를 이용한 API연동을 주로 담당하였으며 
+                                    카페24를 이용한 쇼핑몰 디자인과 리액트를 이용한 농업 ERP 프로젝트 참여경험또한 있습니다.
+                                </p>
+                            </span>
                         </li>
                     </ul>
                     <ul className="gj-about-list grid">
@@ -92,19 +107,6 @@ class Main extends Component {
                                     null
                                 }
                             </ul>
-                        </li>
-                    </ul>
-                </article>
-                <article className="gj-main-layout gj-contact" id="gjContact">
-                    <h1>CONTACT</h1>
-                    <ul>
-                        <li>
-                            <strong>E-mail : </strong>
-                            <p>gonnajoong@gmail.com</p>
-                        </li>
-                        <li>
-                            <strong>Phone : </strong>
-                            <p>010-3290-6667</p>
                         </li>
                     </ul>
                 </article>
